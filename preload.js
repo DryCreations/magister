5e7bd1ad7db1db861d783b8c527c7710dd4576ef
Hayden Mankin <DryCreations@gmail.com>
2021-03-28 18:42:51 -0400

    add unmax feature

--- preload.js
+++ preload.js
@@ -1,15 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
+// https://stackoverflow.com/questions/57807459/how-to-use-preload-js-properly-in-electron
 contextBridge.exposeInMainWorld(
     "api", {
         send: (channel, data) => {
-            let validChannels = ["close", "minimize", "maximize"];
+            let validChannels = ["close", "minimize", "maximize", "unmaximize"];
             if (validChannels.includes(channel)) {
                 ipcRenderer.send(channel, data);
             }
         },
         receive: (channel, func) => {
-            let validChannels = [];
+            let validChannels = ["maximize", "unmaximize"];
             if (validChannels.includes(channel)) {
                 ipcRenderer.on(channel, (event, ...args) => func(...args));
             }
